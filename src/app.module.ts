c1b2ae9e5857535b077f40290c63dc6d88dcfef9
joao <joaosb004@gmail.com>
2023-08-30 16:57:00 -0300

    feat/ all boilerplate done

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,24 @@
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { CredentialsModule } from "./credentials/credentials.module";
+import { NotesModule } from "./notes/notes.module";
+import { CardsModule } from "./cards/cards.module";
+import { EraseModule } from "./erase/erase.module";
+import { PrismaService } from "./prisma/prisma.service";
+import { PrismaModule } from "./prisma/prisma.module";
 
 @Module({
-  imports: [],
+  imports: [
+    UsersModule,
+    CredentialsModule,
+    NotesModule,
+    CardsModule,
+    EraseModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [AppService, PrismaService],
 })
 export class AppModule {}
